26a835247f16150d30e19a2a4cc54621a8b416f0
gtDataLab <gregg.tyson@gmail.com>
2018-05-08 22:59:04 -0400

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+	if (err) {
+		return console.log('Unable to connect to MongoDB server');
+	}
+	console.log('Connected to MongoDB server');
+	const db = client.db('TodoApp');
+
+	// db.collection('Todos').findOneAndUpdate({
+	// 	_id: new ObjectID('5af25d25741bb18cb7284a2d')
+	// }, {
+	// 	$set: {
+	// 		completed: true
+	// 	}
+	// }, {
+	// 	returnOriginal: false
+	// }).then((result) => {
+	// 	console.log(result);
+	// });
+
+		db.collection('Users').findOneAndUpdate({
+		_id: new ObjectID('5af252aa3cf7453d1c22ef16')
+	}, {
+		$set: {
+			name: 'Gregg'
+		},
+		$inc: {
+			age: 1
+		}
+	}, {
+		returnOriginal: false
+	}).then((result) => {
+		console.log(result);
+	});
+
+	// client.close();
+});
